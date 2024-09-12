6edf26d8c97af97e4e8533c35ad2077beefed94b
Don Anderson <don@yourdomain.com>
2024-09-12 15:42:29 -0500

    level-2 work

--- SKYJO/strat-level2.scm
+++ SKYJO/strat-level2.scm
@@ -19,9 +19,12 @@
 ; === Steps for the Level2 strategy ===
 ; The second tier strategy that follows the rules and makes some round-level strategic decisions.
 ;
+; ######################
+; ###### PHASE 1 #######
+; ######################
 ; 1) On the first-round two-flip, open any two cards in separate columns.
 
-;    ---- If you have more that one hidden card ----
+;    ---- If you have more than one hidden card (BASE-PLAY) ----
 ; 2) Using the discard complete a matching column if possible.
 ; 3) If the discard is lower than the highest open card and the highest open card is 5 or greater then replace it.
 ; 4) If the discard is 5 or lower, then replace any hidden card.
@@ -31,7 +34,7 @@
 ; 8) If the draw card is 5 or lower, then replace any hidden card.
 ; 9) Otherwise; discard it.
 
-;    ---- If you have only one hidden card ----
+;    ---- If you have only one hidden card (END-PLAY) ----
 ; 10) Estimate your hand value, by adding all the open cards plus 5 for the hidden card.
 ; 11) Estimate your opponents hand values, by adding up their open cards plus 5 points for each hidden card.
 ;
@@ -50,6 +53,10 @@
 ; 21) If the discard is 5 or lower, replace: the highest open card.
 ; 22) Draw a card and replace the highest open card.
 
+; ######################
+; ###### PHASE 2 #######
+; ######################
+; --> BASE-PLAY
 
 (define (strat-level2 player cmd)
 
@@ -59,11 +66,11 @@
 
         ; Returns #t if a play was executed or #f otherwise
         ((= cmd *strat-cmd-play-phase1*)
-                (strat-level2-any-phase player))
+                (strat-level2-phase1 player))
         
         ; Returns #t if a play was executed or #f otherwise.
         ((= cmd *strat-cmd-play-phase2*)
-                (strat-level2-any-phase player))
+                (strat-level2-phase2 player))
 
         ; Returns a list of the card ids or #f otherwise.
         ((= cmd *strat-cmd-flip-two*)
@@ -87,22 +94,27 @@
 )
 
 ; Returns #t if a play was executed #f otherwise
-(define (strat-level2-any-phase player)
+(define (strat-level2-phase2 player)
+    (BASE-PLAY player)
+)
+
+; Returns #t if a play was executed #f otherwise
+(define (strat-level2-phase1 player)
     (cond
-        ;    ---- If you have more that one hidden card ----
+        ;    ---- If you have more than one hidden card ----
         ((> (player-api-num-cards-hidden player) 1)            
-            (strat-level2-more-than-one-hidden-card player))
+            (BASE-PLAY player))
 
         ;    ---- If you have only one hidden card ----
         ((= (player-api-num-cards-hidden player) 1)            
-            (strat-level2-only-one-hidden-card player))
+            (END-PLAY player))
 
         (else #f)
     )                 
 )
 
 ; Returns #t if a play was executed #f otherwise
-(define (strat-level2-more-than-one-hidden-card player)
+(define (BASE-PLAY player)
     (define highest-open-card-idx (player-api-get-highest-open-card-idx player))
     (define highest-open-card-val 
         (if highest-open-card-idx (player-api-get-open-card-value player highest-open-card-idx) #f))
@@ -154,7 +166,7 @@
 )
 
 ; Returns #t if a play was executed #f otherwise
-(define (strat-level2-only-one-hidden-card player)
+(define (END-PLAY player)
 
         ; 10) Estimate your hand value, by adding all the open cards plus 5 for the hidden card.
         (define my-hand-value-estimate (player-hand-value-estimate player))
